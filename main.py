bfdbcc3bcd3f2b1e3e384da963c65618839f08d3
user0481 <egisxxegis@gmail.com>
2021-11-22 11:16:24 +0200

    visualize c/g dist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from Bio import SeqIO
+import matplotlib.pyplot as plot
+import pylab
 
 
 ENCODINGS = [
@@ -49,7 +51,7 @@ def hand_read_fastq(file_name):
             elif state == state_rating:
                 temp_record.rating = line
                 the_return.append(temp_record)
-                temp_record = HandRecord
+                temp_record = HandRecord()
                 state = state_id
     return the_return
 
@@ -68,6 +70,30 @@ def identify_encoding(rating: str, options: [(str, str, str)]):
     return to_return
 
 
+def calc_distribution(source: str, what: [str]):
+    total = len(source)
+    the_sum = 0
+    for i in range(0, len(what)):
+        the_sum += source.count(what[i])
+    return the_sum / total
+
+
+def prepare_plot(feed_count):
+    coord_min_max_x = [0, 100]
+    coord_min_max_y = [1, feed_count]
+
+    # axis limits
+    pylab.xlim(coord_min_max_x)
+    pylab.ylim(coord_min_max_y)
+
+    plot.title('C/G nukleotidų pasiskirstymas read\'uose')
+    plot.xlabel('C/G nukleotidų dalis read\'e')
+    plot.ylabel('Read\'ų skaičius')
+
+    # show values on contour
+    plot.rcParams.update({'font.size': 12})
+
+
 def main():
     global ENCODINGS
     file_name = "data\\reads_for_analysis.fastq"
@@ -82,6 +108,15 @@ def main():
     encoding_index = counter.index(max(counter))  # if there are multi answers, we pick the top one (leftist)
     encoding = ENCODINGS[encoding_index]
     print("Quality rating encoding: " + encoding[0])
+
+    # calculate cg - s and vizualize
+    the_xs = [100 * calc_distribution(sq.seq, ["C", "G", "S"]) for sq in seqs]
+    the_ys = [read for read in range(1, len(seqs) + 1)]
+    prepare_plot(len(the_xs))
+    plot.plot(the_xs, the_ys)
+    plot.show()
+
+    # find pika
 
     # for seq_record in SeqIO.parse(file_name, "fastq"):
     #     # print(seq_record.letter_annotations["phred_quality"])
--- test.py
+++ test.py
@@ -3,11 +3,15 @@ from main import *
 test_counter = 0
 
 
-def print_if_false(title, boolean):
+def print_if_not_equal(title, left, right):
     global test_counter
     test_counter += 1
-    if not boolean:
-        print("----- TEST. " + title + " failed.")
+    if left != right:
+        print("----- TEST. " + title + " failed: ")
+        print(f"----------- {left}")
+        print(f"-----------  !=")
+        print(f"----------- {right}")
+        print("-----------------------------------------")
         test_counter -= 1
         return
 
@@ -16,31 +20,48 @@ if __name__ == "__main__":
     data = hand_read_fastq("data\\reads_for_analysis.fastq")
     out = len(data)
     answer = 25749
-    print_if_false("number of seqs read", out == answer)
-    data = ''
+    print_if_not_equal("number of seqs read", out, answer)
 
     data = "!$$H"
     param = [("Sanger Phred+33", "!", "I"), ("Solexa Solexa+64", ";", "h")]
     out = identify_encoding(data, param)
     answer = [("Sanger Phred+33", "!", "I")]
-    print_if_false("solexa +33 and +64", out == answer)
+    print_if_not_equal("solexa +33 and +64", out, answer)
 
     data = "!$$H"
     param = [("Sanger Phred+33", "!", "I"), ("Illumina 1.8+ Phred+33", "!", "J")]
     out = identify_encoding(data, param)
     answer = param
-    print_if_false("solexa +33 illumina 1.8+ +33", out == answer)
+    print_if_not_equal("solexa +33 illumina 1.8+ +33", out, answer)
 
     data = "=>?"
     param = [("Sanger Phred+33", "!", "I"), ("Illumina 1.8+ Phred+33", "!", "J"), ("Illumina 1.5+ Phred+64", "B", "i")]
     out = identify_encoding(data, param)
     answer = [("Sanger Phred+33", "!", "I"), ("Illumina 1.8+ Phred+33", "!", "J")]
-    print_if_false("solexa +33 and illumina 1.8+ +33 out of three", out == answer)
+    print_if_not_equal("solexa +33 and illumina 1.8+ +33 out of three", out, answer)
 
     data = "g"
     param = [("Sanger Phred+33", "!", "I"), ("Illumina 1.8+ Phred+33", "!", "J"), ("Illumina 1.5+ Phred+64", "B", "i")]
     out = identify_encoding(data, param)
     answer = [("Illumina 1.5+ Phred+64", "B", "i")]
-    print_if_false("solexa +33 and illumina 1.8+ +33 out of three", out == answer)
+    print_if_not_equal("solexa +33 and illumina 1.8+ +33 out of three", out, answer)
+
+    data = "GACTGACT"
+    param = ["G", "C"]
+    out = calc_distribution(data, param)
+    answer = 0.5
+    print_if_not_equal("50 dist", out, answer)
+
+    data = "GSCTGSCT"
+    param = ["G", "C", "S"]
+    out = calc_distribution(data, param)
+    answer = 3/4
+    print_if_not_equal("3/4 dist", out, answer)
+
+    data = "GAAAAA"
+    param = ["C"]
+    out = calc_distribution(data, param)
+    answer = 0
+    print_if_not_equal("0 dist", out, answer)
 
     print(f"Number of successful tests: {test_counter}")
